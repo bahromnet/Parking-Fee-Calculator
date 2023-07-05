edbd2d182efeecf5c0745655a7f3c7d07c90597e
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-05 16:39:43 +0300

    Add project files.

---
+++ ParkingLot/FeesCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot;
+public class FeesCalculator
+{
+    public int EnteranceFee { get; set; }
+    public int FirstOurFee { get; set; }
+    public int SubsiquentOurFee { get; set; }
+    public string TimeFormat { get; set; }
+}
---
+++ ParkingLot/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkingLot;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
