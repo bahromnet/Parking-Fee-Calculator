8e4134e5a6b4bff440f534db7eaace9606abc268
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-05 00:39:48 +0500

    fixed some error from testing

--- ParkingLot.Tests/UnitTest1.cs
+++ ParkingLot.Tests/UnitTest1.cs
@@ -1,24 +1,26 @@
-using Microsoft.Extensions.Configuration;
-using Moq;
-using Xunit;
+using System.Configuration;
 
 namespace ParkingLot.Tests;
 
 public class UnitTest1
 {
+    FeesCalculator calc;
+
+    public UnitTest1()
+    {
+        ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap
+        {
+            ExeConfigFilename = @"C:\Users\Akramov_Dev\source\repos\ParkingLot\ParkingLot.Tests\App.config" // Update with the correct path
+        };
+        Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+
+        calc = new FeesCalculator();
+        calc.Initialize(configuration);
+    }
 
     [Fact]
-    public static void PositiveTest()
+    public void PositiveTest()
     {
-        Mock<IConfiguration> mockConfig = new Mock<IConfiguration>();
-        mockConfig.Setup(c => c["EnteranceFee"]).Returns("5");
-        mockConfig.Setup(c => c["FirstHourFee"]).Returns("3");
-        mockConfig.Setup(c => c["SubsiquentHourFee"]).Returns("2");
-        mockConfig.Setup(c => c["TimeFormat"]).Returns("HH:mm");
-
-        FeesCalculator calc = new();
-        calc.Initialize();
-
         int actualReasult = calc.GetParkingFee("09:01", "10:00");
         int expectedResult = 5;
         Assert.Equal(expectedResult, actualReasult);
